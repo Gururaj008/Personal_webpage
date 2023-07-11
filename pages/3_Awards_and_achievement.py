bbbdadf8627e2559b072e4dd027b846c50bbd8cf
Gururaj H C <gururaj008@gmail.com>
2023-07-11 15:55:46 +0530

    3_Awards_and_achievement.py

--- pages/3_Awards_and_achievement.py
+++ pages/3_Awards_and_achievement.py
@@ -6,20 +6,22 @@ with col502:
 st.write('')
 st.write('')
 st.write('')
-col504, col505 = st.columns([2,8])
-with col504:
-    st.markdown(':orange[During schooling]')
-with col505:
-    st.markdown('Stood first in class contistently throught out')
-st.write('')
-st.write('')
-col506, col507 = st.columns([2,8])
-with col506:
-    st.markdown(':orange[During Bachelor\'s]')
-with col507:
-    st.markdown('<div style="text-align: justify"> Project work carried out in the final semester was recognized and funded by the premier research institute “Indian Institute of Science, Bengaluru”. Completed the degree with "First Class with Distinction" grade. </div>', unsafe_allow_html=True)
-st.write('')
-st.write('')
+col514, col515 = st.columns([2,8])
+with col514:
+    st.markdown(':orange[At Guvi, IIT-Madras]')
+with col513:
+    st.markdown('<div style="text-align: justify"> Stood "First" in the cumilative test conducted at the end of the Data Science course </div>', unsafe_allow_html=True)
+
+col512, col513 = st.columns([2,8])
+with col512:
+    st.markdown(':orange[At Present work]')
+with col513:
+    st.markdown('<div style="text-align: justify"> Continuing my research in the field of "Wind energy" authored 06 technical papers which are published with SPRINGER, IEEE and other scopus indexed journals. </div>', unsafe_allow_html=True)
+    st.write('')
+    st.markdown('<div style="text-align: justify"> Reviewed the manuscripts submitted for publication with IEEE </div>', unsafe_allow_html=True)
+    st.write('')
+    st.markdown('<div style="text-align: justify"> Consistently recieved "Excellent" rating from students with respect to content preparation and delivery </div>', unsafe_allow_html=True)
+
 col508, col509 = st.columns([2,8])
 with col508:
     st.markdown(':orange[During Master\'s]')
@@ -39,27 +41,37 @@ with col511:
     st.markdown('<div style="text-align: justify"> #Spot recognition award from MFG/PRO Merrimack valley team </div>', unsafe_allow_html=True)
 st.write('')
 st.write('')
-col512, col513 = st.columns([2,8])
-with col512:
-    st.markdown(':orange[At Present work]')
-with col513:
-    st.markdown('<div style="text-align: justify"> Continuing my research in the field of "Wind energy" authored 06 technical papers which are published with SPRINGER, IEEE and other scopus indexed journals. </div>', unsafe_allow_html=True)
-    st.write('')
-    st.markdown('<div style="text-align: justify"> Reviewed the manuscripts submitted for publication with IEEE </div>', unsafe_allow_html=True)
-    st.write('')
-    st.markdown('<div style="text-align: justify"> Consistently recieved "Excellent" rating from students with respect to content preparation and delivery </div>', unsafe_allow_html=True)
+
+col504, col505 = st.columns([2,8])
+with col504:
+    st.markdown(':orange[During schooling]')
+with col505:
+    st.markdown('Stood first in class contistently throught out')
+st.write('')
+st.write('')
+col506, col507 = st.columns([2,8])
+with col506:
+    st.markdown(':orange[During Bachelor\'s]')
+with col507:
+    st.markdown('<div style="text-align: justify"> Project work carried out in the final semester was recognized and funded by the premier research institute “Indian Institute of Science, Bengaluru”. Completed the degree with "First Class with Distinction" grade. </div>', unsafe_allow_html=True)
+st.write('')
+st.write('')
+
+
+
+
     
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    st.write('')
-    col901, col902, col903 = st.columns(3)
-    with col903:
-        st.markdown('_An_ _effort_ _by_ : :yellow[**MAVERICK_GR**]')   
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+st.write('')
+col901, col902, col903 = st.columns(3)
+with col903:
+        st.markdown('_An_ _effort_ _by_ : :orange[**MAVERICK_GR**]')   
     
     
     
