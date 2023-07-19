b5d0e3d4669e25dcf4b8542053f378bdfafb6c16
Gururaj H C <gururaj008@gmail.com>
2023-07-19 12:22:01 +0530

    Home.py

--- Home.py
+++ Home.py
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
 
     #<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="gururaj-hc-data-science-enthusiast" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/gururaj-hc-data-science-enthusiast?trk=profile-badge">Gururaj H C</a></div>
+    #<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="gururaj-hc-data-science-enthusiast" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/gururaj-hc-data-science-enthusiast?trk=profile-badge">Gururaj H C</a></div>
           
     st.write('')
     st.write('')
