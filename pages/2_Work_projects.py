d817c813f503094317b59dfa6cfd6489bc50d7cc
Gururaj H C <gururaj008@gmail.com>
2025-04-10 10:59:42 +0530

    Update and rename 2_Work_projects to 2_Work_projects.py

---
+++ pages/2_Work_projects.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import webbrowser 
+from PIL import Image
+
+if __name__ == '__main__':
+    st.set_page_config(layout="wide")
+    st.markdown("""
+                    <style>
+                    @import url('https://fonts.googleapis.com/css2?family=Agdasima');
+                    .custom-text { font-family: 'Agdasima', sans-serif; font-size: 45px;color:cyan }
+                    </style>
+                    <h1 class="custom-text"> Data Science projects carried out</h1>
+                    """, unsafe_allow_html=True)
+
+
+
+    # col33, col34, col35 = st.columns([3,100,3])
+    # with col34:
+    st.subheader(':orange[**Stackadapt**]')
+    st.write('')
+    st.markdown('<div style="text-align: justify"> I conducted an in-depth analysis of CRM sales KPIs to classify representatives into performance tiers, which helped uncover key success factors and informed targeted coaching strategies that ultimately boosted sales effectiveness. Using Pandas, I performed comprehensive exploratory data analysis (EDA) on revenue, customer, and regional retention metrics, ensuring a detailed understanding of performance trends. I leveraged PostgreSQL for reliable data storage and built dynamic Power BI visualizations to effectively communicate actionable insights to stakeholders. This project enabled me to hone my skills in data munging, data manipulation, statistical analysis, and CRM analytics, while also showcasing my ability to generate and present strategic insights through data visualization.
+.</div>', unsafe_allow_html=True)
+    st.write('')
