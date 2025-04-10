49aa07e43219df7769a05a5dc4d9189f4373d0e7
Gururaj H C <gururaj008@gmail.com>
2025-04-10 11:17:36 +0530

    Update 2_Work_projects.py

--- pages/2_Work_projects.py
+++ pages/2_Work_projects.py
@@ -18,6 +18,15 @@ if __name__ == '__main__':
     # with col34:
     st.subheader(':orange[**Stackadapt**]')
     st.write('')
-    st.markdown('<div style="text-align: justify"> I conducted an in-depth analysis of CRM sales KPIs to classify representatives into performance tiers, which helped uncover key success factors and informed targeted coaching strategies that ultimately boosted sales effectiveness. Using Pandas, I performed comprehensive exploratory data analysis (EDA) on revenue, customer, and regional retention metrics, ensuring a detailed understanding of performance trends. I leveraged PostgreSQL for reliable data storage and built dynamic Power BI visualizations to effectively communicate actionable insights to stakeholders. This project enabled me to hone my skills in data munging, data manipulation, statistical analysis, and CRM analytics, while also showcasing my ability to generate and present strategic insights through data visualization.
-.</div>', unsafe_allow_html=True)
+    st.markdown(
+    '''
+    <div style="text-align: justify">
+        I conducted an in-depth analysis of CRM sales KPIs to classify representatives into performance tiers, which helped uncover key success factors and informed targeted coaching strategies that ultimately boosted sales effectiveness.<br><br>
+        Using Pandas, I performed comprehensive exploratory data analysis (EDA) on revenue, customer, and regional retention metrics, ensuring a detailed understanding of performance trends.<br><br>
+        I leveraged PostgreSQL for reliable data storage and built dynamic Power BI visualizations to effectively communicate actionable insights to stakeholders.<br><br>
+        This project enabled me to hone my skills in data munging, data manipulation, statistical analysis, and CRM analytics, while also showcasing my ability to generate and present strategic insights through data visualization.
+    </div>
+    ''',
+    unsafe_allow_html=True
+    )
     st.write('')
